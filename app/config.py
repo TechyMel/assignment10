fff865cdddf56539aaeaa0697b0768252a2b6e33
mtemu <melvinatemu@gmail.com>
2026-07-11 15:41:49 -0400

    fun testing

--- app/config.py
+++ app/config.py
@@ -1,7 +1,7 @@
 from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
-    DATABASE_URL: str = "sqlite:///./fastapi.db"
+    DATABASE_URL: str = "postgresql://postgres:postgres@localhost:5432/fastapi_db"
     
     class Config:
         env_file = ".env"
